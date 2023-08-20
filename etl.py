defcbe4dfd7b073d3a31f4fb2f66ed195290ad72
Sagar Pednekar <sagar.pednekar@accionlabs.com>
2023-08-21 05:14:31 +0530

    Fix: Added documentation for functions

--- etl.py
+++ etl.py
@@ -1,9 +1,11 @@
+"""Module import."""
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Fetch data from s3 bucket & dump into staging table."""
     for query in copy_table_queries:
         print('Loading staging tables...')
         cur.execute(query)
@@ -12,6 +14,7 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """Query and pouplate fact and dimession tables."""
     for query in insert_table_queries:
         print('Loading fact and dimenssion tables...')
         cur.execute(query)
@@ -20,11 +23,13 @@ def insert_tables(cur, conn):
 
 
 def main():
+    """Establish database connection and perform ELT(extract load and transform)."""
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(
-        *config['CLUSTER'].values()))
+    conn = psycopg2.connect(f"host={config['CLUSTER']['HOST']} \
+                            dbname={config['CLUSTER']['DB_NAME']} user={config['CLUSTER']['DB_USER']} \
+             password={config['CLUSTER']['DB_PASSWORD']} port={config['CLUSTER']['DB_PORT']}")
     cur = conn.cursor()
 
     load_staging_tables(cur, conn)
